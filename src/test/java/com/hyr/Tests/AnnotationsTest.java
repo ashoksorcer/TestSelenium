0dcca53760acb302d016d84d6e6711d35cfc90ae
Ashok_B01 <Ashok_B01@MYSHEC312283L.ad.infosys.com>
2024-02-18 21:54:19 -0500

    Revert "added revert textx"
    This reverts commit ed794c181abad19f0bf09d08b6c000fe7ce3c9cd.

--- src/test/java/com/hyr/Tests/AnnotationsTest.java
+++ src/test/java/com/hyr/Tests/AnnotationsTest.java
@@ -23,7 +23,7 @@ public class AnnotationsTest {
   
   @Test
   public void testMethod3() {
-	  System.out.println("Test Method3 for feature branch - Ashok testing pull revert");
+	  System.out.println("Test Method3 for feature branch - Ashok testing pull");
   }
   
   @BeforeMethod
